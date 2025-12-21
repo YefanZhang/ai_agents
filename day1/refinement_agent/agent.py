54c95899177f0b1f009c68a5bd1d17eb0b8f0e97
Yefan Zhang <yefanzhang@Yefans-MacBook-Pro.local>
2025-12-20 20:16:33 -0800

    update rest of day 1 assignment

---
+++ day1/refinement_agent/agent.py
@@ -0,0 +1,46 @@
+from google.adk.agents import Agent, SequentialAgent, ParallelAgent, LoopAgent
+from google.adk.models.google_llm import Gemini
+from google.adk.runners import InMemoryRunner
+from google.adk.tools import AgentTool, FunctionTool, google_search
+from google.genai import types
+
+retry_config=types.HttpRetryOptions(
+    attempts=5,  # Maximum retry attempts
+    exp_base=7,  # Delay multiplier
+    initial_delay=1,
+    http_status_codes=[429, 500, 503, 504], # Retry on these HTTP errors
+)
+
+# This agent runs ONCE at the beginning to create the first draft.
+initial_writer_agent = Agent(
+    name="InitialWriterAgent",
+    model=Gemini(
+        model="gemini-2.5-flash-lite",
+        retry_options=retry_config
+    ),
+    instruction="""Based on the user's prompt, write the first draft of a short story (around 100-150 words).
+    Output only the story text, with no introduction or explanation.""",
+    output_key="current_story",  # Stores the first draft in the state.
+)
+
+# This agent's only job is to provide feedback or the approval signal. It has no tools.
+critic_agent = Agent(
+    name="CriticAgent",
+    model=Gemini(
+        model="gemini-2.5-flash-lite",
+        retry_options=retry_config
+    ),
+    instruction="""You are a constructive story critic. Review the story provided below.
+    Story: {current_story}
+    
+    Evaluate the story's plot, characters, and pacing.
+    - If the story is well-written and complete, you MUST respond with the exact phrase: "APPROVED"
+    - Otherwise, provide 2-3 specific, actionable suggestions for improvement.""",
+    output_key="critique",  # Stores the feedback in the state.
+)
+
+# This is the function that the RefinerAgent will call to exit the loop.
+def exit_loop():
+    """Call this function ONLY when the critique is 'APPROVED', indicating the story is finished and no more changes are needed."""
+    return {"status": "approved", "message": "Story approved. Exiting refinement loop."}
+
